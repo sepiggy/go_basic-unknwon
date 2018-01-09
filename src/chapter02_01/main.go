892ae431b84248a9071c5b9c6eaed265d02655ee
贾明盛 <jms1209@qq.com>
2018-01-09 20:44:37 +0800

    Initial commit

---
+++ src/chapter02_01/main.go
@@ -0,0 +1,7 @@
+package main
+
+import std "fmt"
+
+func main() {
+	std.Println("使用 package 别名！")
+}
---
+++ src/chapter02_02/main.go
@@ -0,0 +1,7 @@
+package main
+
+import . "fmt"
+
+func main() {
+	Println("使用 package 省略调用!")
+}
